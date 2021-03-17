f56350f5438f9d2d1f804c8f4efbb1b32c45fc0b
m-88888888 <mochi.h8888@gmail.com>
2021-03-18 00:37:11 +0900

    Update Q3.1 to add flag option

--- ch03/ex01/file_copy.go
+++ ch03/ex01/file_copy.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"os"
 )
 
+var flagvar string
+
+func init() {
+	flag.StringVar(&flagvar, "s", "new.txt", "enter the copy file name")
+}
+
 func main() {
+	flag.Parse()
+
 	file, err := os.Open("old.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
-	newFile, err := os.Create("new.txt")
+	newFile, err := os.Create(flagvar)
 	if err != nil {
 		panic(err)
 	}
