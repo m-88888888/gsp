5f6b7f948b8ea0e9fbcc323fe434a1d56ad8c462
m-88888888 <mochi.h8888@gmail.com>
2021-04-10 15:40:08 +0900

    Add answer to Q3.6

---
+++ ch03/ex06/stream.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+var (
+	computer    = strings.NewReader("COMPUTER")
+	system      = strings.NewReader("SYSTEM")
+	programming = strings.NewReader("PROGRAMMING")
+)
+
+func main() {
+	var stream io.Reader
+
+	// ここにioパッケージを使ったコード
+	a := io.NewSectionReader(programming, 5, 1)
+	s := io.LimitReader(programming, 1)
+	c := io.LimitReader(computer, 1)
+	i := io.NewSectionReader(programming, 7, 1)
+	i2 := io.NewSectionReader(programming, 7, 1)
+
+	stream := io.MultiReader(a, s, c, i, i2)
+	io.Copy(os.Stdout, stream)
+}
